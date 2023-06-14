f2d9f07fba4d7f44fa3a5a7c8e4ea60751461748
akaseoyoung <think000412@gmail.com>
2023-06-14 18:20:14 +0900

    구글 맵으로 변경

--- lib/screens/recordPage.dart
+++ lib/screens/recordPage.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RecordPage extends StatefulWidget{
+  const RecordPage({Key? key}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState(){
-    return _RecordPage();
-  }
+  _RecordPageState createState() => _RecordPageState();
 }
 
-class _RecordPage extends State<RecordPage>{
+class _RecordPageState extends State<RecordPage>{
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('record page'),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('record page'),
+          backgroundColor: Colors.lime,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
