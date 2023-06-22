b4244cdf1b95378d0148ea1a004911ab884c6f67
akaseoyoung <think000412@gmail.com>
2023-06-22 16:55:29 +0900

    설정 페이지 자체 코드로 변경, 캘린더 페이지 수정

--- lib/screens/calendarPage.dart
+++ lib/screens/calendarPage.dart
@@ -1,7 +1,14 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'goalPage.dart';
 import '../utils.dart';
+
+class Event {
+  final DateTime date;
+  Event({required this.date});
+}
 
 class CalendarPage extends StatefulWidget{
   const CalendarPage({super.key});
@@ -14,6 +21,7 @@ class _CalendarPageState extends State<CalendarPage>{
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  List<String> days = ['_', '월', '화', '수', '목', '금', '토', '일'];
 
   @override
   Widget build(BuildContext context){
@@ -21,40 +29,55 @@ class _CalendarPageState extends State<CalendarPage>{
       appBar: AppBar(
         title: Text('산책 달력'),
       ),
-      body: TableCalendar(
-        firstDay: kFirstDay,
-        lastDay: kLastDay,
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          // Use `selectedDayPredicate` to determine which day is currently selected.
-          // If this returns true, then `day` will be marked as selected.
-
-          // Using `isSameDay` is recommended to disregard
-          // the time-part of compared DateTime objects.
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            // Call `setState()` when updating the selected day
-            setState(() {
-              _selectedDay = selectedDay;
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+              titleCentered: true,
+            ),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              // 사용자가 날짜를 선택했을 때
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                // Call `setState()` when updating the selected day
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                // Call `setState()` when updating calendar format
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              // No need to call `setState()` here
               _focusedDay = focusedDay;
-            });
-          }
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            // Call `setState()` when updating calendar format
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          // No need to call `setState()` here
-          _focusedDay = focusedDay;
-        },
+            },
+            calendarBuilders: CalendarBuilders(
+              dowBuilder: (context, day) {
+                return Center(child: Text(days[day.weekday],));
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+            child: Text(
+              '기록을 확인할 날짜를 선택해주세요'
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/screens/settingPage.dart
+++ lib/screens/settingPage.dart
@@ -1,80 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:settings_ui/settings_ui.dart';
 
-class SettingPage extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState(){
-    return _SettingPage();
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+  static const String _title = '설정';
+  static const List<String> _data = [
+    '계정',
+    '알림',
+    '앱 정보',
+    '로그아웃',
+  ];
+  static const List<IconData> _icon = [
+    Icons.account_circle,
+    Icons.notifications,
+    Icons.info,
+    Icons.logout,
+  ];
+
+  Widget _settingListView() {
+    return ListView.builder(
+      itemCount: _data.length,
+      itemBuilder: (BuildContext context, int i) {
+        return InkWell(
+          onTap: (){},
+          child: ListTile(
+            title: Text(_data[i],
+            style: TextStyle(
+              fontSize: 16
+            ),),
+            trailing: Icon(_icon[i]),
+          ),
+        );
+      },
+    );
   }
-}
 
-class _SettingPage extends State<SettingPage>{
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-// <<<<<<< HEAD
-//       // backgroundColor: Colors.lime,
-// =======
-// >>>>>>> c9419eb5e09f7e6b7c0afc3ee2d666e65cb67057
-      appBar: AppBar(
-        title: Text('설정'),
-      ),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            title: Text(
-              '공통',
-            ),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.language),
-                title: Text('언어'),
-                value: Text('한국어'),
-                onPressed: ((context) {}),
-              ),
-              // SettingsTile.switchTile(
-              //   title: Text('진동'),
-              //   initialValue: vibration,
-              //   onToggle: (value) {
-              //     setState(() {
-              //       vibration = !vibration;
-              //     });
-              //   },
-              //   leading: Icon(Icons.vibration),
-              // ),
-            ],
-          ),
-          SettingsSection(
-            title: Text('개인정보 수정'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.account_circle),
-                title: Text('나의 정보'),
-                onPressed: ((context) {}),
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text('계정'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.logout),
-                title: Text('로그아웃'),
-                onPressed: ((context) {}),
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text('기타'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.star),
-                title: Text('앱 평가하기'),
-                onPressed: ((context) {}),
-              ),
-            ],
-          ),
-        ],
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(_title),
+          elevation: 0,
+          backgroundColor: Colors.lime,
+        ),
+        body: _settingListView(),
       ),
     );
   }
