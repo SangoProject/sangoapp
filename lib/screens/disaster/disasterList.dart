93fa7e262f605e5fedad612b5a15a77eae9afc35
chaeyeon0101 <moodi0320@gmail.com>
2023-11-09 18:27:19 +0900

    재난 정보 띄우기

--- lib/screens/disaster/disasterList.dart
+++ lib/screens/disaster/disasterList.dart
@@ -1,6 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:sangoproject/screens/disaster/disasterMsg.dart';
+import 'package:sangoproject/screens/disaster/disasterMsgRepository.dart';
 
 class DisasterListPage extends StatelessWidget {
+  DisasterListPage({super.key});
+  DisasterRepository _disasterRepository = DisasterRepository();
+
+  Widget _makeMsgOne(DisasterMsg disasterMsg) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "create_date : " + disasterMsg.create_date.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "location_name : " + disasterMsg.location_name.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "msg : " + disasterMsg.msg.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    final bannerheight = MediaQuery.of(context).size.height;
+    final bannerwidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '재난 문자 공지',
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: _disasterRepository.loadDisaster(),
+          builder: (context, AsyncSnapshot<List<DisasterMsg>?> snapshot) {
+            if (snapshot.hasData == false) {
+              return CircularProgressIndicator();
+            }
+            return Expanded(
+              child: Container(
+                child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: 20,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        //width: bannerwidth,
+                        //height: MediaQuery.of(context).size.height,
+                        margin: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Colors.white, // 하얀 배경
+                          borderRadius: BorderRadius.circular(8), // 약간의 BorderRadius
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5), // 그림자 색상 및 투명도
+                              spreadRadius: 2, // 그림자 확장 범위
+                              blurRadius: 5, // 그림자 흐림 범위
+                              offset: Offset(0, 3), // 그림자 위치 (x, y)
+                            ),
+                          ],
+                        ),
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: _makeMsgOne(snapshot.data![index]),
+                        ),
+                      );
+                    }
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+/*class DisasterListPage extends StatelessWidget {
   const DisasterListPage({super.key});
 
   @override
@@ -19,8 +122,8 @@ class DisasterListPage extends StatelessWidget {
         ),
         body: Container(
           child: ListView.builder(
-              scrollDirection: Axis.vertical, // 스크롤 방향을 가로로 설정
-              itemCount: 15, // 10개의 아이템을 만듭니다
+              scrollDirection: Axis.vertical,
+              itemCount: 15,
               itemBuilder: (context, index) {
                 // 각 아이템은 정사각형 모양의 컨테이너입니다
                 return Container(
@@ -55,4 +158,4 @@ class DisasterListPage extends StatelessWidget {
         )
     );
   }
-}
+}*/
